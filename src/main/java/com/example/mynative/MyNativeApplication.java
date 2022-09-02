79d55d5c5ea5185979e673aa3da573b06b679df4
up1 <somkiat.p@gmail.com>
2022-09-02 11:57:05 +0700

    Init demo

---
+++ src/main/java/com/example/mynative/MyNativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mynative/MyNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
